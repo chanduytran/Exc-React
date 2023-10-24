4d8df35f8dca33ed4a3f2292d5706f1c2a47a25e
yud <chanduytran51@gmail.com>
2023-10-24 11:08:01 +0700

    Components 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent'; 
+
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Menu /> {/* Include your Menu component here */}
     </div>
   );
 }
